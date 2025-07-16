dc4236a50ed04b4f76da6c3a6672d756119981c5
efddrsn <80856309+efddrsn@users.noreply.github.com>
2025-07-16 15:44:06 -0300

    Add src files and public assets

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import AgendaPage from './pages/AgendaPage';
+import ConsultationModePage from './pages/ConsultationModePage';
+import PacientesPage from './pages/PacientesPage';
+import OrcamentosPage from './pages/OrcamentosPage';
+import FinanceiroPage from './pages/FinanceiroPage';
+import EstoquePage from './pages/EstoquePage';
+import ComunicacaoPage from './pages/ComunicacaoPage';
+import RelatoriosPage from './pages/RelatoriosPage';
+import ContabilPage from './pages/ContabilPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Modo consulta - fora do layout padrão para experiência mobile otimizada */}
+        <Route path="/consultation-mode" element={<ConsultationModePage />} />
+        
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="agenda" element={<AgendaPage />} />
+          <Route path="pacientes" element={<PacientesPage />} />
+          <Route path="orcamentos" element={<OrcamentosPage />} />
+          <Route path="financeiro" element={<FinanceiroPage />} />
+          <Route path="estoque" element={<EstoquePage />} />
+          <Route path="comunicacao" element={<ComunicacaoPage />} />
+          <Route path="relatorios" element={<RelatoriosPage />} />
+          <Route path="contabil" element={<ContabilPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
